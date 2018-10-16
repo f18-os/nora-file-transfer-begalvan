066dfb9fb3d0fea2193741468a2b06b5d688424f
begalvan <begalvan@miners.utep.edu>
2018-10-16 02:23:51 -0600

    init thread lock

--- thread-lab/fileThreadServer.py
+++ thread-lab/fileThreadServer.py
@@ -4,3 +4,10 @@ import socket
 import os
 from _thread import *
 import threading 
+
+num_of_clients = 5 #listen to up to 5 clients
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((socket.gethostname(), 50001))
+server.listen(num_of_clients) 
+text_file = 'client_' + str(1) + '.txt'
+lock = threading.Lock() #initialize lock
