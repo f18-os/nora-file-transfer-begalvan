98fb0ee8a9b720fcfab98e196eb667a16853d275
begalvan <begalvan@miners.utep.edu>
2018-10-16 02:26:23 -0600

    rec output file

--- thread-lab/fileThreadServer.py
+++ thread-lab/fileThreadServer.py
@@ -11,3 +11,29 @@ server.bind((socket.gethostname(), 50001))
 server.listen(num_of_clients) 
 text_file = 'client_' + str(1) + '.txt'
 lock = threading.Lock() #initialize lock
+
+i = 1
+while i <= num_of_clients:
+    c, addr = server.accept()
+    lock.acquire()
+    print("\nconnection successful with client " +
+            str(i) + str(addr) + "\n")
+    text_file = 'client_' + str(i) + '.txt'
+
+    # Receive, output and save file
+    with open(text_file, "wb") as fw: #opens file and returns stream
+        print("Receiving..")
+        while True:
+            print('receiving')
+            data = c.recv(100) #receive up to 100 bytes from socket
+            if data == b'start': #bytes-start
+                continue
+            elif data == b'end':
+                print('Breaking from file write')
+                break
+            else:
+                decoded_data = data.decode("utf-8")
+                if not decoded_data:
+                    print("\nconnection with client " + str(i) + " broken\n")
+                    print("  CLIENT " + str(i) + " -> " + decoded_data)
+                    break
